3cb733dd105c1a213dda69ad0b3879136d26f052
Bryan <bssandovalrod@gmail.com>
2020-11-06 12:50:43 -0600

    headers

--- index.php
+++ index.php
@@ -2,12 +2,12 @@
 require('vendor/autoload.php');
 
 header('Access-Control-Allow-Origin: *');
-header("Content-Type: application/json; charset=UTF-8");
-header("Access-Control-Allow-Methods: OPTIONS,GET,POST,PUT,DELETE");
-header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
+header("Content-Type: application/json");
 
-$uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
-$uri = explode( '/', $uri );
+/* $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+$uri = explode( '/', $uri ); */
 
   $input = (array) json_decode(file_get_contents('php://input'), TRUE);
   $email = new \SendGrid\Mail\Mail();
