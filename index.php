df2e0bac5933491d4b8485ee6c5a448c1ea5278b
Bryan <bssandovalrod@gmail.com>
2020-11-06 15:11:26 -0600

    API ready

--- index.php
+++ index.php
@@ -2,19 +2,11 @@
 require('vendor/autoload.php');
 
 header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept");
-header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE');
-header("Content-Type: application/json");
-
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-  echo "HOLA";
-  http_response_code(201);
-  return 'hola';
-}
+header("Access-Control-Allow-Headers: Access-Control-Allow-Headers, Access-Control-Allow-Origin, Content-Type");
 
 /* $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 $uri = explode( '/', $uri ); */
-
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   $input = (array) json_decode(file_get_contents('php://input'), TRUE);
   $email = new \SendGrid\Mail\Mail();
   $email->setFrom($input['from']);
@@ -24,13 +16,18 @@ $uri = explode( '/', $uri ); */
   }
   $email->addTo($input['to']);
   $email->addContent("text/html", $input['html']);
+  if(!isset($input['api_key'])){
+    echo "API KEY REQUIRED";
+    return true;
+  }
   $sendgrid = new \SendGrid($input['api_key']);
 
   try {
     $response = $sendgrid->send($email);
-    return $response->statusCode() . "\n";
-    return($response->headers());
-    return $response->body() . "\n";
+    print_r ($response->statusCode() . "\n");
+    print_r ($response->headers());
+    print_r ($response->body() . "\n");
   } catch (Exception $e) {
-    return 'Caught exception: '. $e->getMessage() ."\n";
+    print_r ('Caught exception: '. $e->getMessage() ."\n");
   }
+}
